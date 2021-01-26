7dd107067146c3d368f722fd17b4586591acbb28
frontendprof <https://github.com/frontendprof>
2021-01-27 00:10:33 +0500

    hooks with context

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,39 +1,29 @@
 
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 
 
-class BookList extends Component {
+const BookList = () => {
 
-    
+    const {isLightTheme,light,dark}=useContext(ThemeContext);
+    const theme=isLightTheme?light:dark;
 
-    render() {
-        
-        return (
-            <ThemeContext.Consumer>{context=>{
-                const {isLightTheme,light,dark}=context;
-                const theme=isLightTheme?light:dark;
+    return (
+        <div>
+            <div className="book-list" style={{ color:theme.syntax, background:theme.bg }}>
 
-                return(
-                    <div className="book-list" style={{ color:theme.syntax, background:theme.bg }}>
+                <ul>
+                    <li style={{ background:theme.ui }}>Road To Mecca</li>
+                    <li style={{ background:theme.ui }}>The Divine Reality</li>
+                    <li style={{ background:theme.ui }}>On  Being Human</li>
+                </ul>
 
-                        <ul>
-                            <li style={{ background:theme.ui }}>Road To Mecca</li>
-                            <li style={{ background:theme.ui }}>The Divine Reality</li>
-                            <li style={{ background:theme.ui }}>On  Being Human</li>
-                        </ul>
-                
-                    </div>
-                )
-
-                
-            }}
-             
-            </ThemeContext.Consumer>
-           
-        )
-    }
+            </div>  
+        </div>
+    )
 }
+
+
 
 export default BookList
